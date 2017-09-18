a200fde9b3f98c5522138976b55eada0f8a7a0ae
aquin018 <32046532+aquin018@users.noreply.github.com>
2017-09-17 19:37:51 -0700

    First Commit - HWK1
    hwk1 for CS 485 game programming

---
+++ HWK1 - CS 485/Assets/Scripts/Not Complete - CameraMouseControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseControl : MonoBehaviour {
+	public GameObject playerpt2;
+	float horizontalSpeed = 2.0f;
+	float verticalSpeed = 2.0f;
+
+	private Vector3 offset;
+
+	//intialization for the camera
+	void Start () {
+		offset = transform.position - playerpt2.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		 if (Input.GetAxis("Fire2") != 0) 
+			{
+			float h = horizontalSpeed * Input.GetAxis ("Mouse Y");
+			float v = verticalSpeed * Input.GetAxis ("Mouse X");
+			 	transform.Translate(v,h,0);
+			}	
+		
+	}
+
+	void LateUpdate () {
+		transform.position = playerpt2.transform.position + offset;
+	}
+}
